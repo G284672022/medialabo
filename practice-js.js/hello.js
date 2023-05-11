e9635cec1035e20f7d4071fa5c0eddc5f3e3ed34
G284672022 <105624259+G284672022@users.noreply.github.com>
2023-05-11 15:08:02 +0900

    a

--- practice-js.js/hello.js
+++ practice-js.js/hello.js
@@ -1,5 +1,16 @@
 console.log('こんにちは');
- let x
- x=3
- x=x+1
- x
+ let x;
+ x=3;
+ x=x+1;
+ x;
+
+ 
+     let cnt = 0;            // グローバル変数
+function foo(s) {
+    console.log(s);
+    cnt = cnt + 1;      // グローバル変数に1加える
+}
+
+foo('おはよう');
+foo('こんにちは');
+console.log(cnt);       // 2を出力
