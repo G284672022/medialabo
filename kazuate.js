a7970d8a050b536f9fd62e3abbbbf2637aa5ff40
G284672022 <105624259+G284672022@users.noreply.github.com>
2023-06-01 18:37:03 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -3,7 +3,8 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu=1;
+
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -17,35 +18,44 @@ function greeting() {
 }
 
 hantei();
-hantei();
-hantei();
-hantei();
+
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  let l =document.querySelector('span#answer');
+  l.textContent = yoso;
+  let s =document.querySelector('span#kaisu');
+  s.textContent=kaisu;
+  let r =document.querySelector('p#result');
   
-  for(let i=1;i<10;i++){
-    console.log(i+"回目の予想:"+yoso);
-    if(yoso===kotae){
-     console.log("正解です．おめでとう!");
-     break;
+  if(kotae===yoso){
+    if(kaisu<=2){
+      r.textContent='正解です.おめでとう!';
+      kaisu=kaisu+4;
+    }else if(kaisu===3){
+      r.textContent='正解です.おめでとう!';
+      kaisu++;
+    }else if(kaisu>=4){
+      r.textContent='答えは '+kotae+' でした．すでにゲームは終わっています';
+      kaisu++;
     }
-    if(i<3){
-         if(yoso<kotae){
-           console.log("まちがい．答えはもっと大きいですよ");
-         }else if(yoso>kotae){
-           console.log("まちがい．答えはもっと小さいですよ");
-         }
-     }else if(i===3){
-         console.log("まちがい．残念でした答えは "+kotae+" です．")
-     }else if(i>=4){
-         console.log("答えは "+kotae+" でした．すでにゲームは終わっています");
-     }
-
- }
-    
+  }else if(kotae!=yoso){
+    if(kaisu<=2){
+      if(kotae<yoso){
+        r.textContent='まちがい．答えはもっと小さいですよ';
+      }
+      if(kotae>yoso){
+        r.textContent='まちがい．答えはもっと大きいですよ';
+      }
+    }else if(kaisu===3){
+      r.textContent='まちがい．残念でした答えは '+kotae+' です．';
+    }else{
+      r.textContent='答えは '+kotae+' でした．すでにゲームは終わっています';
+    }
+    kaisu++;
+  }  
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
