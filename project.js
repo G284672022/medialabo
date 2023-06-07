a71bf9af4e4598574ce5e9683b19a2a98a7c88c3
G284672022 <105624259+G284672022@users.noreply.github.com>
2023-06-08 02:42:04 +0900

    プロジェクト課題

---
+++ project.js
@@ -0,0 +1,59 @@
+
+let b = document.querySelector('#print');
+b.addEventListener('click', sendRequest);
+
+function sendRequest() {
+  let i=document.querySelector('input[name="name"]');
+  let kunimei=i.value;
+
+  if(kunimei==='エジプト'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/360630.json';}
+  if(kunimei==='ロシア'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/524901.json';}
+  if(kunimei==='南アフリカ'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/993800.json';}
+  if(kunimei==='中国'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1816670.json';}
+  if(kunimei==='日本'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1850147.json';}
+  if(kunimei==='シンガポール'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1880252.json';}
+  if(kunimei==='オーストラリア'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2147714.json';}
+  if(kunimei==='イギリス'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2643743.json';}
+  if(kunimei==='フランス'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2968815.json';}
+  if(kunimei==='ブラジル'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/3451189.json';}
+  if(kunimei==='アメリカ'){
+  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/5128581.json';}
+  
+  axios.get(url)
+        .then(showResult)
+        .catch(showError)   
+}
+function showResult(resp){
+  let data = resp.data;
+  let a =document.querySelector('th#a');
+  a.textContent=data.coord.lon;
+  let b =document.querySelector('th#b');
+  b.textContent=data.coord.lat;
+  let c =document.querySelector('th#c');
+  c.textContent=data.weather.description;
+  let d =document.querySelector('th#d');
+  d.textContent=data.main.temp_min;
+  let e =document.querySelector('th#e');
+  e.textContent=data.main.temp_max;
+  let f =document.querySelector('th#f');
+  f.textContent=data.main.humidity;
+  let g =document.querySelector('th#g');
+  g.textContent=data.wind.speed;
+  let h =document.querySelector('th#h');
+  h.textContent=data.wind.deg;
+  let i =document.querySelector('th#i');
+  i.textContent=data.name;
+}
+function showError(err) {
+  console.log(err);
+}
