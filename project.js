1c7086cdd7bca09eab09e9c77fe504001ef40704
G284672022 <105624259+G284672022@users.noreply.github.com>
2023-06-09 14:51:55 +0900

    project課題

--- project.js
+++ project.js
@@ -2,36 +2,54 @@
 let b = document.querySelector('#print');
 b.addEventListener('click', sendRequest);
 
+let p=document.querySelector('tr#data');
+let q=document.querySelector('tr#shousai');
+p.style.fontSize="0px";
+q.style.fontSize="0px";
+
+
 function sendRequest() {
+
+  var n=document.getElementById("data");
+  var m=document.getElementById("shousai");
+  var r=document.getElementById('border');
+  r.style.borderColor='black';
+  n.style.borderColor='black';
+  m.style.borderColor='black';
+	n.style.fontSize="16.5px";
+  m.style.fontSize="16.5px";
+
   let i=document.querySelector('input[name="name"]');
   let kunimei=i.value;
+  let url;
 
   if(kunimei==='エジプト'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/360630.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/360630.json';}
   if(kunimei==='ロシア'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/524901.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/524901.json';}
   if(kunimei==='南アフリカ'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/993800.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/993800.json';}
   if(kunimei==='中国'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1816670.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1816670.json';}
   if(kunimei==='日本'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1850147.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1850147.json';}
   if(kunimei==='シンガポール'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1880252.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/1880252.json';}
   if(kunimei==='オーストラリア'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2147714.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2147714.json';}
   if(kunimei==='イギリス'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2643743.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2643743.json';}
   if(kunimei==='フランス'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2968815.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/2968815.json';}
   if(kunimei==='ブラジル'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/3451189.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/3451189.json';}
   if(kunimei==='アメリカ'){
-  let url ='https://www.nishita-lab.org/web-contents/jsons/openweather/5128581.json';}
+   url ='https://www.nishita-lab.org/web-contents/jsons/openweather/5128581.json';}
+  
   
   axios.get(url)
         .then(showResult)
-        .catch(showError)   
+        .catch(showError); 
 }
 function showResult(resp){
   let data = resp.data;
@@ -40,7 +58,7 @@ function showResult(resp){
   let b =document.querySelector('th#b');
   b.textContent=data.coord.lat;
   let c =document.querySelector('th#c');
-  c.textContent=data.weather.description;
+  c.textContent=data.weather[0].description;
   let d =document.querySelector('th#d');
   d.textContent=data.main.temp_min;
   let e =document.querySelector('th#e');
